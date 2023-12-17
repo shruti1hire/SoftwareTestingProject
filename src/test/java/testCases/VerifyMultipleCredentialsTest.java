50f848804f9a566203bc9391e4f49052857a3eb1
Shrutika Hire <141499803+shrutikahire@users.noreply.github.com>
2023-12-17 20:25:41 +0530

    Update VerifyMultipleCredentialsTest.java

--- src/test/java/testCases/VerifyMultipleCredentialsTest.java
+++ src/test/java/testCases/VerifyMultipleCredentialsTest.java
@@ -29,7 +29,6 @@ public class VerifyMultipleCredentialsTest extends TestBase
 		return new Object[] []
 				{
 					{"standard_user","secret_sauce"}, 
-					{"locked_out_user","secret_sauce"},
 					{"problem_user","secret_sauce"}, 
 					{"performance_glitch_user","secret_sauce"}, 
 					{"error_user","secret_sauce"}, 
