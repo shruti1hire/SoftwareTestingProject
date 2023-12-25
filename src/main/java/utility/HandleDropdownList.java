5b793a2a71beefe48a0bd102aea309e912d270c8
Shrutika Hire <141499803+shrutikahire@users.noreply.github.com>
2023-12-25 12:53:39 +0530

    Update HandleDropdownList.java

--- src/main/java/utility/HandleDropdownList.java
+++ src/main/java/utility/HandleDropdownList.java
@@ -5,8 +5,8 @@ import org.openqa.selenium.support.ui.Select;
 
 public class HandleDropdownList 
 {
-	//public bz i wnt to use that method throughout the project then static bz i dnt this method face any issue in other class 
-		public static void handleSelectClass(WebElement ele,String value) //in ele=dropdown n value=price(low to high)
+	
+		public static void handleSelectClass(WebElement ele,String value) 
 		{
 			Select sc = new Select(ele);
 			sc.selectByVisibleText(value);
